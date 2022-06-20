2fac10c02a35c96797ca7a3e9851f0e80b157cf2
GeigerTiger55 <elise.geiger@gmail.com>
2022-06-20 12:09:33 -0700

    created model for cupcake

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, redirect, flash
+#from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "SECRET!"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,36 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DEFAULT_IMAGE = 'https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg'
+
+def connect_db(app):
+    """Connect this database to Flask app"""
+
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Class for a cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+    flavor = db.Column(
+        db.String(50),
+        nullable=False)
+    size = db.Column(
+        db.String(50),
+        nullable=False)
+    rating = db.Column(
+        db.Integer,
+        nullable=False)
+    image = db.Column(
+        db.Text,
+        nullable=False,
+        default=DEFAULT_IMAGE)
